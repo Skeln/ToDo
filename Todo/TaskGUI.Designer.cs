5f2af41df9515dd5d688c0f2cca5204bcf6b53e9
axlwaii <markus.waitl@absolventa.de>
2014-10-04 15:36:03 +0200

    improve gui

--- Todo/TaskGUI.Designer.cs
+++ Todo/TaskGUI.Designer.cs
@@ -28,43 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.addTaskButton = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.SuspendLayout();
-            // 
-            // addTaskButton
-            // 
-            this.addTaskButton.Location = new System.Drawing.Point(523, 13);
-            this.addTaskButton.Name = "addTaskButton";
-            this.addTaskButton.Size = new System.Drawing.Size(134, 23);
-            this.addTaskButton.TabIndex = 0;
-            this.addTaskButton.Text = "Add task";
-            this.addTaskButton.UseVisualStyleBackColor = true;
-            // 
-            // panel1
-            // 
-            this.panel1.AutoScroll = true;
-            this.panel1.Location = new System.Drawing.Point(12, 52);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(645, 326);
-            this.panel1.TabIndex = 1;
-            // 
-            // TaskGUI
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(669, 390);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.addTaskButton);
-            this.Name = "TaskGUI";
-            this.Text = "ToDo-Application";
-            this.ResumeLayout(false);
+      this.addTaskButton = new System.Windows.Forms.Button();
+      this.mainTaskPanel = new System.Windows.Forms.Panel();
+      this.SuspendLayout();
+      // 
+      // addTaskButton
+      // 
+      this.addTaskButton.BackColor = System.Drawing.Color.MediumSeaGreen;
+      this.addTaskButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+      this.addTaskButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.addTaskButton.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.addTaskButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+      this.addTaskButton.Location = new System.Drawing.Point(523, 13);
+      this.addTaskButton.Name = "addTaskButton";
+      this.addTaskButton.Size = new System.Drawing.Size(134, 33);
+      this.addTaskButton.TabIndex = 0;
+      this.addTaskButton.Text = "Add task";
+      this.addTaskButton.UseVisualStyleBackColor = false;
+      // 
+      // mainTaskPanel
+      // 
+      this.mainTaskPanel.AutoScroll = true;
+      this.mainTaskPanel.BackColor = System.Drawing.SystemColors.ControlLight;
+      this.mainTaskPanel.Location = new System.Drawing.Point(12, 64);
+      this.mainTaskPanel.Name = "mainTaskPanel";
+      this.mainTaskPanel.Size = new System.Drawing.Size(645, 314);
+      this.mainTaskPanel.TabIndex = 1;
+      // 
+      // TaskGUI
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.SystemColors.ButtonFace;
+      this.ClientSize = new System.Drawing.Size(669, 390);
+      this.Controls.Add(this.mainTaskPanel);
+      this.Controls.Add(this.addTaskButton);
+      this.Name = "TaskGUI";
+      this.Text = "ToDo-Application";
+      this.MouseEnter += new System.EventHandler(this.FocusPanel);
+      this.MouseHover += new System.EventHandler(this.FocusPanel);
+      this.ResumeLayout(false);
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button addTaskButton;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel mainTaskPanel;
     }
 }
