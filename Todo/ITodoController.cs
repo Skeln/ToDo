2f0ab01d6f4fd4954ea4572c58037777be0ff568
axlwaii <markus.waitl@absolventa.de>
2014-10-15 11:50:33 +0200

    add todo interface, - fachkonzept interface

---
+++ Todo/ITodoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo
+{
+    interface ITodoController
+    {
+
+        int saveMainTask(string subject, string description);
+        int saveMainTask(int id, string subject, string description);
+        int saveSubTask(string subject);
+        int saveSubTask(int id, string subject);
+
+        List<MainTask> getAllMainTasks();
+
+        void checkMainTask(int id);
+        void checkSubTask(int id);
+
+        void deleteSubTask(int subTaskID);
+        void deleteMainTask(int mainTaskID);
+        void deleteAllTasks();
+
+    }
+}
