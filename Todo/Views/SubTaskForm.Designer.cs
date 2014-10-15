03bb5ae6944e99a402f0d4c4689107da045ad5fe
axlwaii <markus.waitl@absolventa.de>
2014-10-15 11:49:59 +0200

    add subtask form

---
+++ Todo/Views/SubTaskForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Todo.Views
+{
+    partial class SubTaskForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.saveSubTaskBtn = new System.Windows.Forms.Button();
+            this.subjectTextBox = new System.Windows.Forms.TextBox();
+            this.subjectLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelBtn.Location = new System.Drawing.Point(118, 40);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(75, 23);
+            this.cancelBtn.TabIndex = 9;
+            this.cancelBtn.Text = "cancel";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            this.cancelBtn.Click += new System.EventHandler(this.cancelBtn_Click);
+            // 
+            // saveSubTaskBtn
+            // 
+            this.saveSubTaskBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveSubTaskBtn.Location = new System.Drawing.Point(199, 40);
+            this.saveSubTaskBtn.Name = "saveSubTaskBtn";
+            this.saveSubTaskBtn.Size = new System.Drawing.Size(75, 23);
+            this.saveSubTaskBtn.TabIndex = 8;
+            this.saveSubTaskBtn.Text = "save";
+            this.saveSubTaskBtn.UseVisualStyleBackColor = true;
+            this.saveSubTaskBtn.Click += new System.EventHandler(this.saveSubTaskBtn_Click);
+            // 
+            // subjectTextBox
+            // 
+            this.subjectTextBox.Location = new System.Drawing.Point(61, 12);
+            this.subjectTextBox.Name = "subjectTextBox";
+            this.subjectTextBox.Size = new System.Drawing.Size(213, 20);
+            this.subjectTextBox.TabIndex = 7;
+            // 
+            // subjectLabel
+            // 
+            this.subjectLabel.AutoSize = true;
+            this.subjectLabel.Location = new System.Drawing.Point(11, 15);
+            this.subjectLabel.Name = "subjectLabel";
+            this.subjectLabel.Size = new System.Drawing.Size(43, 13);
+            this.subjectLabel.TabIndex = 6;
+            this.subjectLabel.Text = "Subject";
+            // 
+            // SubTaskForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 75);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.saveSubTaskBtn);
+            this.Controls.Add(this.subjectTextBox);
+            this.Controls.Add(this.subjectLabel);
+            this.Name = "SubTaskForm";
+            this.Text = "SubTaskForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cancelBtn;
+        private System.Windows.Forms.Button saveSubTaskBtn;
+        private System.Windows.Forms.TextBox subjectTextBox;
+        private System.Windows.Forms.Label subjectLabel;
+    }
+}
---
+++ Todo/Views/SubTaskForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Todo.Views
+{
+    partial class SubTaskForm : Form
+    {
+
+        private MainTaskControl _mainTaskControl;
+
+        public SubTaskForm(MainTaskControl mc)
+        {
+            _mainTaskControl = mc;
+            InitializeComponent();
+        }
+
+        private void saveSubTaskBtn_Click(object sender, EventArgs e)
+        {
+                //TODO: this should go into the controller 
+            SubTask st = new SubTask(this.subjectTextBox.Text, _mainTaskControl.CtrlMainTask.ID);
+            _mainTaskControl.AddSubTaskComponent(st);
+
+            this.Dispose();
+
+        }
+
+        private void cancelBtn_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+    }
+}
