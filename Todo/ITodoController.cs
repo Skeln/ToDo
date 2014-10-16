4852cb58064b639c21954cf1dbfbe6793976ef94
axlwaii <markus.waitl@absolventa.de>
2014-10-16 18:52:09 +0200

    add get methods for tasks to interface

--- Todo/ITodoController.cs
+++ Todo/ITodoController.cs
@@ -9,12 +9,15 @@ namespace Todo
     interface ITodoController
     {
 
+
         int SaveMainTask(string subject, string description);
         int SaveMainTask(int id, string subject, string description);
         int SaveSubTask(string subject, int mainTaskID);
         int SaveSubTask(int id, string subject);
 
         List<MainTask> GetAllMainTasks();
+        MainTask GetMainTask(int mainTaskID);
+        SubTask GetSubTask(int subTaskID);
 
         void CheckMainTask(int id);
         void CheckSubTask(int id);
