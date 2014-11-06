d90a48347a558d57ef95f3ff7f8112c7fc97783b
axlwaii <markus.waitl@absolventa.de>
2014-11-06 12:29:53 +0100

    add abstract task class

---
+++ Todo/Task.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Todo
+{
+    abstract class Task
+    {
+        public int ID { get; set; }
+        public string Subject { get; set; }
+        public virtual bool Done { get; set; }
+    }
+}
